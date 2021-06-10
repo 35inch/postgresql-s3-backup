49c89421c40d6ed6a7a7b463a247b37b78d32722
selcuk.yildirim <selcuk.yildirim@35inch.com>
2021-06-10 08:55:16 +0300

    add two s3 support

--- README.md
+++ README.md
@@ -30,6 +30,18 @@ Postgresql tabanlı olarak yapılmıştır fakat diğer veri tabanlarına da ent
 
  S3_STORAGE_CLASS = STANDARD or GLACIER (optinal)
  
+
+ S3_REGION_2 = s3 region
+ 
+ S3_HOST_2 = s3 host (optinal)
+ 
+ S3_ACCESS_KEY_2 = s3 access key
+ 
+ S3_SECRET_KEY_2 = s3 secret key
+ 
+ S3_BUCKET_2 = s3 bucket
+
+ S3_STORAGE_CLASS_2 = STANDARD or GLACIER (optinal)
  
  Example cronjob yaml:
 
--- backup.sh
+++ backup.sh
@@ -29,5 +29,27 @@ if [[ -z "${S3_STORAGE_CLASS}" ]]; then
 else
     AWS_ARGS2="--storage-class=${S3_STORAGE_CLASS}"
 fi
-
+echo "Uploading to S3 1"
 aws s3 cp $file s3://$S3_BUCKET/$file $AWS_ARGS $AWS_ARGS2
+
+
+if [[ -z "${S3_ACCESS_KEY_2}" ]]; then
+    export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_2
+    export AWS_SECRET_ACCESS_KEY=$S3_SECRET_KEY_2
+    export AWS_DEFAULT_REGION=$S3_REGION_2
+
+    if [[ -z "${S3_HOST}" ]]; then
+        AWS_ARGS=""
+    else
+        AWS_ARGS="--endpoint-url=${S3_HOST_2}"
+    fi
+
+    if [[ -z "${S3_STORAGE_CLASS_2}" ]]; then
+        AWS_ARGS2=""
+    else
+        AWS_ARGS2="--storage-class=${S3_STORAGE_CLASS_2}"
+    fi
+    echo "Uploading to S3 2"
+
+    aws s3 cp $file s3://$S3_BUCKET_2/$file $AWS_ARGS $AWS_ARGS2
+fi
